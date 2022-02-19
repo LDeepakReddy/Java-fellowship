032520c4c8313cdce2c668c56700477dedc6dc40
LDeepakReddy <98506486+LDeepakReddy@users.noreply.github.com>
2022-02-19 05:41:19 +0530

    Update multiplication.java

--- src/com/blz/multiplication.java
+++ src/com/blz/multiplication.java
@@ -3,8 +3,8 @@ package com.blz;
 public class multiplication {
     public static void main(String[] args) {
         System.out.println();
-        int a = 100, b = 12, multiplication;
-        multiplication = a * b;
+        int a = 100, b = 12, c = 5, multiplication;
+        multiplication = (a * b) * c ;
         System.out.println("multiplication of a and b " + multiplication);
     }
 }
